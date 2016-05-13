ffa381bd7e7ed949e044e237de9630b59973ad91
fapspirit <fapspirit@users.noreply.github.com>
2016-05-13 17:38:23 +0300

    Homework 3
    fix map

--- 3.py
+++ 3.py
@@ -36,12 +36,12 @@ class Dot(object):
   def distance(self, other):
     if len(self.point) != len(other.point):
       raise ValueError
-    return sum(imap(lambda x, y: (x - y)**2, self.point, other.point))**0.5
+    return sum(map(lambda x, y: (x - y)**2, self.point, other.point))**0.5
 
   def middle(self, other):
     if len(self.point) != len(other.point):
       raise ValueError
-    return Dot(*list(imap(lambda x, y: (x + y) / 2.0, self.point, other.point)))
+    return Dot(*list(map(lambda x, y: (x + y) / 2.0, self.point, other.point)))
 
 
 ##########
